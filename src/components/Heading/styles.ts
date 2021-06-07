369af790f801036e9c55b65bd335c3956aa76747
Eduardo Lima <dlima78@hotmail.com>
2021-06-07 16:39:41 -0300

    feat: create a Heading component

---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,36 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import media from 'styled-media-query'
+import { HeadingProps } from '.'
+
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      border-bottom: 0.5rem solid ${theme.colors.primary};
+      bottom: -1rem;
+      content: '';
+      left: 0;
+      position: absolute;
+      width: 5rem;
+    }
+  `
+}
+
+export const Wrapper = styled.h2<HeadingProps>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge}
+    `}
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+  `}
+`
