905c808704586a82cc70e9c6e6bcdb1ea540e56d
Eduardo Lima <dlima78@hotmail.com>
2021-06-10 10:25:12 -0300

    feat: allow changing the color and size of the Heading component

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,6 +1,5 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helper'
-import 'jest-styled-components'
 
 import Heading from '.'
 
@@ -22,7 +21,7 @@ describe('<Heading />', () => {
   it('should render a heding with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1'
+      'border-left': '0.7rem solid #F231A5'
     })
   })
 
@@ -35,5 +34,46 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  it('should render a heding with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
+  })
+
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Won Games
+      </Heading>
+    )
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle('border-left: 0.7rem solid #F231A5')
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Won Games
+      </Heading>
+    )
+    const heading = screen.getByRole('heading', { name: /won games/i })
+    expect(heading).toHaveStyle('border-left: 0.7rem solid #3CD3C1')
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
