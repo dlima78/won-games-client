0a020394341c571f5466479522c5c199b9823749
Eduardo Lima <dlima78@hotmail.com>
2021-06-10 19:26:54 -0300

    feat: allow the Button component to behave as a link

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -25,10 +25,6 @@ const wrapperModifiers = {
     width: 100%;
   `,
   withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-
     svg {
       width: 1.5rem;
 
@@ -41,6 +37,9 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     border: none;
     border-radius: ${theme.border.radius};
@@ -48,6 +47,11 @@ export const Wrapper = styled.button<WrapperProps>`
     cursor: pointer;
     outline: none;
     padding: ${theme.spacings.xxsmall};
+    text-decoration: none;
+
+    &:hover {
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+    }
 
     ${!!size && wrapperModifiers[size](theme)}
     ${fullWidth && wrapperModifiers.fullWidth}
