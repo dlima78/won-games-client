666a74be31df108dcfc010d35ac6dd038e6c818b
Eduardo Lima <dlima78@hotmail.com>
2021-06-15 19:47:15 -0300

    feat: create GameCard component

---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,83 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import GameCard from '.'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'popularionzero.jpg',
+  price: 'R$ 215,00'
+}
+
+describe('<GameCard />', () => {
+  it('should render GameCard correctly', () => {
+    const { container } = renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...props} />)
+    const price = screen.getByText('R$ 215,00')
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+    expect(price).toHaveStyle({ backgroundColor: '#3CD3C1' })
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 200,00" />)
+    expect(screen.getByText('R$ 200,00')).not.toHaveStyle({
+      textDecoration: 'line-through'
+    })
+    expect(screen.getByText('R$ 215,00')).toHaveStyle({
+      textDecoration: 'line-through'
+    })
+  })
+
+  it('should render a filled favourite icon when favourite is true', () => {
+    renderWithTheme(<GameCard {...props} favorite />)
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should call onFav method when fovorite is clicked', () => {
+    const onFav = jest.fn()
+    renderWithTheme(<GameCard {...props} favorite onFav={onFav} />)
+    fireEvent.click(screen.getAllByRole('button')[0])
+    expect(onFav).toBeCalled()
+  })
+
+  it('should render a Ribbon', () => {
+    const onFav = jest.fn()
+    renderWithTheme(
+      <GameCard
+        {...props}
+        favorite
+        onFav={onFav}
+        ribbon="20% off"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/20% off/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: 'background: #F231A5' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
+})
