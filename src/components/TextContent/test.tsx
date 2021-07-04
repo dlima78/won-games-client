a49a586781ce1c25398f9392a54f9798279fe29d
Eduardo Lima <dlima78@hotmail.com>
2021-07-04 14:39:53 -0300

    feat: create TextContent component

---
+++ src/components/TextContent/test.tsx
@@ -0,0 +1,49 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import TextContent from '.'
+
+const props = {
+  title: 'Description',
+  content: `<h1>Content</h1>`
+}
+
+describe('<TextContent />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /description/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /content/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(<TextContent content={props.content} />)
+
+    expect(
+      screen.getByRole('heading', { name: /content/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.queryByRole('heading', { name: /description/i })
+    ).not.toBeInTheDocument()
+  })
+
+  it('should render without title', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole('heading', {
+      name: /description/i
+    }).parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    })
+  })
+})
