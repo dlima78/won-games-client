7c79ae891f19bdb543a9971aceef2b39698c94f9
Eduardo Lima <dlima78@hotmail.com>
2021-07-21 16:25:33 -0300

    feat:create Games page

---
+++ src/templates/Games/index.tsx
@@ -0,0 +1,42 @@
+import { KeyboardArrowDown } from '@styled-icons/material-outlined'
+
+import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { Grid } from 'components/Grid'
+import Base from 'templates/Base'
+import * as S from './styles'
+
+export type GamesTemplateProps = {
+  games?: GameCardProps[]
+  filterItems: ItemProps[]
+}
+
+const Games = ({ games = [], filterItems }: GamesTemplateProps) => {
+  const handleFilter = () => {
+    return
+  }
+
+  const handleShowMore = () => {
+    return
+  }
+  return (
+    <Base>
+      <S.Main>
+        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        <section>
+          <Grid>
+            {games.map((item) => (
+              <GameCard key={item.title} {...item} />
+            ))}
+          </Grid>
+          <S.ShowMore role="button" onClick={handleShowMore}>
+            <p>Show more</p>
+            <KeyboardArrowDown size={35} />
+          </S.ShowMore>
+        </section>
+      </S.Main>
+    </Base>
+  )
+}
+
+export default Games
