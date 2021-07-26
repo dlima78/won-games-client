957d6ffa9e0d041010e514471d82ff6a6f4f8904
Eduardo Lima <dlima78@hotmail.com>
2021-07-26 11:28:07 -0300

    feat: create CartDropdown component

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,25 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import CartDropdown, { CartDropdownProps } from '.'
+
+import mockItems from 'components/CartList/mock'
+
+export default {
+  title: 'CartDropdown',
+  component: CartDropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<CartDropdownProps> = (args) => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown {...args} />
+  </div>
+)
+
+Default.args = {
+  items: mockItems,
+  total: 'RS 310,00'
+}
