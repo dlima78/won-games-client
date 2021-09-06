27929fa655c9f91c1406bf4b01e4477466496b3f
Eduardo Lima <dlima78@hotmail.com>
2021-09-06 11:15:18 -0300

    feat: disable the purchase button when there is an error and empty fields

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -8,8 +8,10 @@ import * as S from './styles'
 
 const PaymentForm = () => {
   const [error, setError] = useState<string | null>()
+  const [disabled, setDisabled] = useState(true)
 
   const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setDisabled(event.empty)
     setError(event.error ? event.error.message : '')
   }
   return (
@@ -41,7 +43,11 @@ const PaymentForm = () => {
         <Button as="a" fullWidth minimal>
           Continue a comprar
         </Button>
-        <Button fullWidth icon={<ShoppingCart />}>
+        <Button
+          fullWidth
+          icon={<ShoppingCart />}
+          disabled={disabled || !!error}
+        >
           Buy now
         </Button>
       </S.Footer>
