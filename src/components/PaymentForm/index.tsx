f5fb1c524df2800e6ff64925850d5ee2907c4337
Eduardo Lima <dlima78@hotmail.com>
2021-09-09 10:46:49 -0300

    feat: add payment method

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js'
-import { useCart } from 'hooks/use-cart'
-import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import { PaymentIntent, StripeCardElementChangeEvent } from '@stripe/stripe-js'
 import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
+
+import { useCart } from 'hooks/use-cart'
 import Button from 'components/Button'
 import Heading from 'components/Heading'
+
 import * as S from './styles'
-import { createPaymentIntent } from 'utils/stripe/methods'
+import { createPayment, createPaymentIntent } from 'utils/stripe/methods'
 import { Session } from 'next-auth/client'
 import { FormLoading } from 'components/Form'
 
@@ -18,18 +20,22 @@ type PaymentFormProps = {
 const PaymentForm = ({ session }: PaymentFormProps) => {
   const { items } = useCart()
   const { push } = useRouter()
+  const stripe = useStripe()
+  const elements = useElements()
+
   const [error, setError] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
   const [disabled, setDisabled] = useState(true)
   const [clientSecret, setClientSecret] = useState('')
   const [freeGames, setFreeGames] = useState(false)
-  const stripe = useStripe()
-  const elements = useElements()
 
   useEffect(() => {
     async function setPaymentMode() {
       if (items.length) {
-        const data = await createPaymentIntent({ items, token: session.jwt })
+        const data = await createPaymentIntent({
+          items,
+          token: session.jwt
+        })
 
         if (data.freeGames) {
           setFreeGames(true)
@@ -38,11 +44,10 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
 
         if (data.error) {
           setError(data.error)
-          return
+        } else {
+          setFreeGames(false)
+          setClientSecret(data.client_secret)
         }
-
-        setFreeGames(false)
-        setClientSecret(data.client_secret)
       }
     }
 
@@ -54,20 +59,33 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     setError(event.error ? event.error.message : '')
   }
 
+  const saveOrder = async (paymentIntent?: PaymentIntent) => {
+    const data = await createPayment({
+      items,
+      paymentIntent,
+      token: session.jwt
+    })
+
+    return data
+  }
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
     setLoading(true)
 
     if (freeGames) {
+      saveOrder()
+
       push('/success')
       return
     }
 
     const payload = await stripe!.confirmCardPayment(clientSecret, {
       payment_method: {
-        card: elements!.getElement(CardElement)
+        card: elements!.getElement(CardElement)!
       }
     })
+
     if (payload.error) {
       setError(`Payment failed ${payload.error.message}`)
       setLoading(false)
@@ -75,9 +93,12 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
       setError(null)
       setLoading(false)
 
+      saveOrder(payload.paymentIntent)
+
       push('/success')
     }
   }
+
   return (
     <S.Wrapper>
       <form onSubmit={handleSubmit}>
@@ -85,6 +106,7 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
           <Heading color="black" size="small" lineBottom>
             Payment
           </Heading>
+
           {freeGames ? (
             <S.FreeGames>Only free games, click buy and enjoy!</S.FreeGames>
           ) : (
@@ -103,13 +125,14 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
 
           {error && (
             <S.Error>
-              <ErrorOutline size={20} /> {error}
+              <ErrorOutline size={20} />
+              {error}
             </S.Error>
           )}
         </S.Body>
         <S.Footer>
           <Button as="a" fullWidth minimal>
-            Continue a comprar
+            Continue shopping
           </Button>
           <Button
             fullWidth
@@ -123,4 +146,5 @@ const PaymentForm = ({ session }: PaymentFormProps) => {
     </S.Wrapper>
   )
 }
+
 export default PaymentForm
