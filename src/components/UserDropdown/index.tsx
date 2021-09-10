3bd7599899b04c79817367f0cb7fdc3d2ac5aba7
Eduardo Lima <dlima78@hotmail.com>
2021-09-10 08:44:20 -0300

    refactor: improve signOut and redirect flow

--- src/components/UserDropdown/index.tsx
+++ src/components/UserDropdown/index.tsx
@@ -8,39 +8,49 @@ import {
 } from '@styled-icons/material-outlined'
 import Dropdown from 'components/Dropdown'
 import * as S from './styles'
+import { useRouter } from 'next/router'
 
 export type UserDropdownProps = {
   username: string
 }
 
-const UserDropdown = ({ username }: UserDropdownProps) => (
-  <Dropdown
-    title={
-      <>
-        <AccountCircle size={24} />
-        <S.Username>{username}</S.Username>
-        <ChevronDown size={24} />
-      </>
-    }
-  >
-    <S.Nav>
-      <Link href="/profile/me" passHref>
-        <S.Link>
-          <AccountCircle />
-          <span>My Profile</span>
+const UserDropdown = ({ username }: UserDropdownProps) => {
+  const { push } = useRouter()
+  return (
+    <Dropdown
+      title={
+        <>
+          <AccountCircle size={24} />
+          <S.Username>{username}</S.Username>
+          <ChevronDown size={24} />
+        </>
+      }
+    >
+      <S.Nav>
+        <Link href="/profile/me" passHref>
+          <S.Link>
+            <AccountCircle />
+            <span>My Profile</span>
+          </S.Link>
+        </Link>
+        <Link href="/wishlist" passHref>
+          <S.Link>
+            <FavoriteBorder />
+            <span>Wishlist</span>
+          </S.Link>
+        </Link>
+        <S.Link
+          role="button"
+          onClick={async () => {
+            const data = await signOut({ redirect: false, callbackUrl: '/' })
+            push(data.url)
+          }}
+        >
+          <ExitToApp />
+          <span>Sign out</span>
         </S.Link>
-      </Link>
-      <Link href="/wishlist" passHref>
-        <S.Link>
-          <FavoriteBorder />
-          <span>Wishlist</span>
-        </S.Link>
-      </Link>
-      <S.Link role="button" onClick={() => signOut()}>
-        <ExitToApp />
-        <span>Sign out</span>
-      </S.Link>
-    </S.Nav>
-  </Dropdown>
-)
+      </S.Nav>
+    </Dropdown>
+  )
+}
 export default UserDropdown
